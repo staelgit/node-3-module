bc24b7cf47a0f3fb888f1f1c0cce246a5d3d87c0
Stahnev Yury <stahnev@stael.ru>
2022-11-22 19:12:18 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const yargs = require('yargs');
+const pkg = require('./package.json')
+const { addNote, removeNote, printNotes } = require('./notes.controller')
+
+yargs.version(pkg.version)
+
+yargs.command({
+   command: 'add',
+   describe: 'Add new note to list',
+   builder: {
+      title: {
+         type: 'string',
+         describe: 'Note title',
+         demandOption: true
+      }
+   },
+   handler({ title }) {
+      addNote(title);
+   }
+})
+
+yargs.command({
+   command: 'remove',
+   describe: 'Remove note by id',
+   builder: {
+      id: {
+         type: 'string',
+         describe: 'id of the note to delete',
+         demandOption: true
+      }
+   },
+   handler({ id }) {
+      removeNote(id);
+   }
+})
+
+yargs.command({
+   command: 'list',
+   describe: 'Print all notes',
+   async handler() {
+      printNotes();
+   }
+})
+
+yargs.parse();
